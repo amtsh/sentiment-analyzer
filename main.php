2dea689b4e05c94e2e732a186dacf49e14b9d204
amiit-github <shinde.amit27031991@gmail.com>
2013-06-12 17:56:59 +0530

    Create main.php
    Sentiment analysis

---
+++ main.php
@@ -0,0 +1,72 @@
+
+<?php
+require("pos.php");
+require("neg.php");
+require("negators.php");
+
+/*
+  takes the input and returns the verdict about it.
+*/
+function main($statement) {
+	
+	/*Cleaning the input data*/
+	$statement = preg_replace('/\.+/','.',$statement);	
+	$statement = preg_replace('/http:\/\/\S+/','',$statement);
+
+	/*Calculate the result of the input statement*/
+	$result = sentiment_calc($statement);
+	return $result;
+}
+
+/*			
+			Processes the statement and returns the verdict whether it is a Positive/Negative Response 
+			or just a Neutral statement.
+	
+	*/
+function sentiment_calc($statement) {
+	/*statement is neutral initially.*/
+	$pos_score = 0; 
+	$neg_score = 0;
+	$negator_score = 0;
+	$verdict = '';
+	/*If null statement then return*/
+	if($statement == '' || $statement == ' ') return;
+	/*If multiple statements, pass each statement*/
+	if(strpos($statement, '.') !== false) {
+		$sentences= explode('.', $statement);
+		foreach ($sentences as $each_sentence) {
+				if($each_verdict = sentiment_calc(trim($each_sentence)))
+				$verdict .= $each_verdict.";";
+		}
+		/*return ';'' separated result of multiple statement*/
+		return $verdict;
+	}
+	global $pos_array, $neg_array, $negator_array;
+	
+	if($statement != str_ireplace($negator_array, '', $statement))
+			$negator_score++;
+	if($statement != str_ireplace($pos_array, '', $statement))
+			$pos_score++;
+	if($statement != str_ireplace($neg_array, '', $statement))
+			$neg_score++;
+		
+	
+	/*If statement only contains negators then it is negative response.*/
+	if($negator_score >0) {
+		if($pos_score == 0 && $neg_score == 0) {
+			return $verdict="NEGATIVE";
+		}
+		else {
+			($pos_score > 0)?$pos_score = -1:'';
+			($neg_score > 0)?$neg_score = -1:'';
+		}
+	}
+	/*If statement has more postive words than its postive else negative*/
+	($pos_score>$neg_score)?	$verdict="POSITIVE":$verdict="NEGATIVE";
+	/*If statement has no positive/negative words then neutral*/
+		if($pos_score===$neg_score) {$verdict="NEUTRAL";}
+
+	return $verdict;
+}
+
+?>
